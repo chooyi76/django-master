68779bc7d01167365c65388f228e9e75fe898ffb
chooyi76 <chooyi94@gmail.com>
2022-03-22 15:49:13 +0900

    3-15

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q, Count
 
 from ..models import Question
 
@@ -9,16 +10,33 @@ def index(request):
     pybo 목록 출력
     """
     # 입력 파라미터
-    page = request.GET.get('page', '1')
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')       # 검색어
+    so = request.GET.get('so', 'recent') # 정렬기준
+
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:
+        question_list = Question.objects.order_by('-create_date')
 
     # 조회
-    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            # subject__icontains=kw의 의미는 제목에 kw 문자열이 포함되었는지를 의미한다.
+            Q(subject__icontains=kw) |                  # 제목 검색
+            Q(content__icontains=kw) |                  # 내용 검색
+            Q(author__username__icontains=kw) |         # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)   # 답변 글쓴이 검색
+        ).distinct()
 
     # 페이징 처리
     paginator = Paginator(question_list, 10)  # 페이지 당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 
